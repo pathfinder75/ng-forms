2d8fad73cc74d2ed4418baefa362d77df651afae
CARRERE Yves <SQ5760@d21.tes.local>
2018-06-26 13:39:31 +0200

    add uppercase, autocapitalize, and lowercase feature

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
 import { OnInit } from "@angular/core/src/metadata/lifecycle_hooks";
+var upperCase = require('upper-case');
+declare var $: any;
 
 @Component({
     selector: "app-forms",
@@ -12,19 +14,20 @@ export class FormsComponent {
     }
 
     update(value: string){
-        this.values = this.nomPropre(value);
+        this.values = value.toUpperCase();
+        // this.values = value;
     }
     
-
+    // listen to the keyboard event of the user
     onKeyup(event: any){
-        this.values = this.nomCompose((<HTMLInputElement>event.target).value);
+        // this.values = this.nomPropre((<HTMLInputElement>event.target).value);
     }
 
     reset(){
         this.values = "";
     }
 
-    // format text to Uppercase
+    // format any text to autocapitalize
     public nomPropre(text: string){
         return text.charAt(0).toLocaleUpperCase() + text.slice(1).toLocaleLowerCase();
       }
@@ -35,11 +38,6 @@ export class FormsComponent {
         
         if(separateur.test(resultat)){
             let sep;
-            // let sep = resultat.match(separateur);
-            // for (var elem of sep){
-            //     console.log(elem);
-            //     this.separer(elem, resultat);
-            // }
             console.log(resultat.match(separateur));
             
             resultat.indexOf(" ") > 0 ? sep = " " : resultat.indexOf("-") > 0 ? sep = "-" : resultat.indexOf(".") ? sep = "." : sep = "_";
@@ -57,6 +55,50 @@ export class FormsComponent {
             }
         }
         return resultat;
+    }
+    ngOnInit() {
+        // fonction qui récupère l'attribut autocapitalize de l'input field...
+        $('input').keyup(function(e){
+            console.log(upperCase('iii', 'tr'));
+        });
+        $('input').each(function(i){
+            if($(this).prop("autocapitalize") === "words"){
+                // add the  autocapitalize css class
+                $(this).addClass('autocapitalize');
+            }
+            if($(this).prop("autocapitalize") === "none" || $(this).prop("autocapitalize") === "off")
+            {
+                // leave it as it is as default : do nothing
+            }
+            if($(this).prop("autocapitalize") === "sentences" || $(this).prop("autocapitalize") === "on")
+            {
+                $(this).on("keyup", function(e) {
+                    var inp = String.fromCharCode(e.keyCode);
+                    if (/[a-zA-Z0-9-_ ]/.test(inp)) {
+                        var val = $(this).val().toLocaleUpperCase();
+                        var type = $(this).prop("autocapitalize");
+                        if ( type === "words" || type === "sentences" ) {
+                            var split_identifier = type === "words" ? " " : ". ";
+                            var split = val.split(split_identifier);
+                            $.each(split, function(i, v){
+                                if ( v.length ) {
+                                    split[i] = v.charAt(0).toUpperCase() + v.substring(1);
+                                }
+                            });
+                            val = split.join(split_identifier);
+                        } else if ( type === "characters" ) {
+                            val = upperCase(val);
+                        }
+                        $(this).val(val);
+                    }
+                });
+            }
+            if($(this).prop("autocapitalize") === "characters")
+            {
+                $(this).addClass('uppercase');
+            }
+            
+        });
     }
 
 }
