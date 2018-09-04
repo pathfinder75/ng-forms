7457bfe7df5f330b9a6691b3271e55dac8c8c2da
yves carrere <carrere.yves@gmail.com>
2018-09-04 08:47:31 +0200

    add new msg service

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,16 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'app/services/message.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-message',
-  templateUrl: './message.component.html',
+  // templateUrl: './message.component.html',
+  template: `<div id="message" *ngFor="let message of messages">{{message}}</div>
+  `,
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
 
   constructor(private messageService: MessageService) { }
+  public source: Observable<number>;
+  public messages: String[];
+  get Messages(){
+    return this.messageService.messages;
+  }
 
   ngOnInit() {
+    this.messageService.add('Are you alive ?');
+    this.messageService.subscribeMessage();
+    // fetch messages from messageService
+    this.source = Observable.create((observer) => {
+      observer.next(1)
+      observer.next(2)
+      observer.next(3)
+      observer.complete()
+    });
+
+    
+
+    if(this.messageService.messages.length){
+      const element = document.getElementById('message');
+      const source = Observable.create((observer) => {
+        // this.messages.forEach((item) => observer.next(item));
+      })
+      source.subscribe((v) => console.log(v));
+
+
+    }
   }
 
 }
--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import { of } from 'rxjs/Observable/of';
 
 @Injectable()
 export class MessageService {
-  messages: string[] = [];
+  public messages: string[] = [];
+
+  createObservable():Observable<String[]> {
+    return Observable.create((observer) => {
+      this.messages.forEach((item) => observer.next(item))
+    });
+  }
+  subscribeMessage(){
+    const msg = this.createObservable();
+    msg.subscribe(
+      (data: String[]) => console.log(data)
+    )
+  }
+  displayMessages(msg){
+  }
 
   add(message: string) {
     this.messages.push(message);
